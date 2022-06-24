37ed34e28e6cced432f81f078fd906daf5c9052e
Васил Любенов Младенов <VLMladenov19@codingburgas.bg>
2022-06-24 08:05:48 +0300

    Fix admin user creation

--- src/2122-otj-10-project-VLMladenov19/main.cpp
+++ src/2122-otj-10-project-VLMladenov19/main.cpp
@@ -4,9 +4,5 @@
 
 int main()
 {
-	std::string adminUsername = "admin", adminPass = "adminpass";
-	User admin(adminUsername, adminPass, "", "", 0, Roles::administrator);
-	admin.addUserToFile();
-
 	printMenu();
 }
